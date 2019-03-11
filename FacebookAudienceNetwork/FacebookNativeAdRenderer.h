d70239cb3e8ed16462a03f23fa9b4eea058626f5
“chauduyphanvu1 <vchau@twitter.com>
2019-03-11 17:46:48 -0400

    Update FacebookNativeAdRenderer.h

--- FacebookAudienceNetwork/FacebookNativeAdRenderer.h
+++ FacebookAudienceNetwork/FacebookNativeAdRenderer.h
@@ -22,8 +22,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, readonly) MPNativeViewSizeHandler viewSizeHandler;
 
-+ (MPNativeAdRendererConfiguration *)rendererConfigurationWithRendererSettings:(id<MPStaticNativeAdRendererSettings>)rendererSettings;
-
++ (MPNativeAdRendererConfiguration *)rendererConfigurationWithRendererSettings:(id<MPNativeAdRendererSettings>)rendererSettings;
 @end
 
 NS_ASSUME_NONNULL_END
