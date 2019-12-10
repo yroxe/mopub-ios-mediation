1d4585508466aed2b1882f845f0605852bca6e47
Hikmet Mert Kahraman <hmertkahraman@twitter.com>
2019-12-10 20:17:55 +0000

    ironSource - Adapters release - 6.11.0.0.0

--- IronSource/CHANGELOG.md
+++ IronSource/CHANGELOG.md
@@ -1,4 +1,7 @@
 ## Changelog
+* 6.11.0.0.0
+    * This version of the adapters has been certified with ironSource 6.11.0.0.
+
 * 6.10.0.0.0
     * This version of the adapters has been certified with ironSource 6.10.0.0.
 
--- IronSource/IronSourceAdapterConfiguration.m
+++ IronSource/IronSourceAdapterConfiguration.m
@@ -32,7 +32,7 @@ NSString * const kIronSourceAppkey = @"applicationKey";
 #pragma mark - MPAdapterConfiguration
 
 - (NSString *)adapterVersion {
-    return @"6.10.0.0.0";
+    return @"6.11.0.0.0";
 }
 
 - (NSString *)biddingToken {
