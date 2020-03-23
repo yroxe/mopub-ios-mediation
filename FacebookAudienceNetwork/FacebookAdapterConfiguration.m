5c59a1e27fb105f8a789e008d0df37495aab617d
Roxana Raicu <rroxe@fb.com>
2020-03-23 21:53:18 +0000

    Updated for FAN 5.8.0

--- FacebookAudienceNetwork/FacebookAdapterConfiguration.m
+++ FacebookAudienceNetwork/FacebookAdapterConfiguration.m
@@ -14,7 +14,7 @@
 #import "MPConstants.h"
 #endif
 
-#define FACEBOOK_ADAPTER_VERSION             @"5.7.1.0"
+#define FACEBOOK_ADAPTER_VERSION             @"5.8.0.0"
 #define MOPUB_NETWORK_NAME                   @"facebook"
 
 static NSString * const kFacebookPlacementIDs = @"placement_ids";
