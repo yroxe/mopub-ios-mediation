9799d3390a4ee0bc3871a59d2c18f0ba5077b068
Hikmet Mert Kahraman <hmertkahraman@twitter.com>
2020-04-16 18:55:05 +0300

    ironSource - Adapters release - 6.16.0.0

--- IronSource/IronSourceAdapterConfiguration.m
+++ IronSource/IronSourceAdapterConfiguration.m
@@ -32,7 +32,7 @@ NSString * const kIronSourceAppkey = @"applicationKey";
 #pragma mark - MPAdapterConfiguration
 
 - (NSString *)adapterVersion {
-    return @"6.15.0.1.0";
+    return @"6.16.0.0";
 }
 
 - (NSString *)biddingToken {
