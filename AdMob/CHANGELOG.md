0ab8d4be5aee5dcf1d651b56116226dd4d46de7d
chauduyphanvu1 <vchau@twitter.com>
2020-04-27 18:07:32 -0400

    Google - 7.58.0.1

--- AdMob/CHANGELOG.md
+++ AdMob/CHANGELOG.md
@@ -1,4 +1,7 @@
 ## Changelog
+  * 7.58.0.1
+    * Redefine valid architectures.
+
   * 7.58.0.0
     * This version of the adapters has been certified with AdMob 7.58.0 and MoPub 5.12.1.
 
--- AdMob/GoogleAdMobAdapterConfiguration.m
+++ AdMob/GoogleAdMobAdapterConfiguration.m
@@ -40,7 +40,7 @@ typedef NS_ENUM(NSInteger, AdMobAdapterErrorCode) {
 #pragma mark - MPAdapterConfiguration
 
 - (NSString *)adapterVersion {
-    return @"7.58.0.0";
+    return @"7.58.0.1";
 }
 
 - (NSString *)biddingToken {
