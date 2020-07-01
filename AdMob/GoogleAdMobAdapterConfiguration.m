3116616580e9ea4d139f47d79b7c61ef08cb4e38
Vu <vchau@twitter.com>
2020-07-01 18:31:40 -0400

    Google - 7.61.0.1 (#300)

--- AdMob/GoogleAdMobAdapterConfiguration.m
+++ AdMob/GoogleAdMobAdapterConfiguration.m
@@ -40,7 +40,7 @@ typedef NS_ENUM(NSInteger, AdMobAdapterErrorCode) {
 #pragma mark - MPAdapterConfiguration
 
 - (NSString *)adapterVersion {
-    return @"7.61.0.0";
+    return @"7.61.0.1";
 }
 
 - (NSString *)biddingToken {
